cf50eabbed631f184d5e3ebf9f9584bc1d2d5e6a
jordvnkm <minatogawaj@gmail.com>
2016-07-21 13:08:39 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/main.jsx",
+  output: {
+    path: 'app/assets/javascripts/',
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
